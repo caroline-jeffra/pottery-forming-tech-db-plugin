e933eb40f3ecda92a3443dae787eac1e660717ac
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-11-24 18:37:27 +0100

    create db table on activation via wp hook

--- pottery-forming-tech-db.php
+++ pottery-forming-tech-db.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * Plugin Name:  Learn Register Routes
- * 
+ * Plugin Name: Pottery Forming Technology Database
+ * Plugin URI: https://github.com/caroline-jeffra/pottery-forming-tech-db-plugin
+ * Description: Create and manage a database and associated api of archaeological pottery technology data
+ * Author: Caroline Jeffra
+ * Author URI: https://github.com/caroline-jeffra
+ * Text Domain: pottery-forming-tech-db
+ * License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Version: 1.0
  */
+
+ register_activation_hook( __FILE__, 'pftd_setup_table');
+ function pftd_setup_table() {
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'pottery_ftd_object';
+
+  $sql = "CREATE TABLE $table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    pot_type varchar (20) NOT NULL,
+    forming_method mediumint (9) NOT NULL,
+    shape varchar (100) NOT NULL,
+    catalog_number varchar (100) NOT NULL,
+    traces_observed varchar (255) NOT NULL,
+    PRIMARY KEY (id)
+    )";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+ }
+
 
 ?>
